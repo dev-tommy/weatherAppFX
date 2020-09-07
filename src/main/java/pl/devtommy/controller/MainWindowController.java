d8240e5007d32506170de4da53fb81540833b7f1
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-09-07 18:25:23 +0200

    Design simple weather forecast window

--- src/main/java/pl/devtommy/controller/MainWindowController.java
+++ src/main/java/pl/devtommy/controller/MainWindowController.java
@@ -1,12 +1,23 @@
 package pl.devtommy.controller;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 
 public class MainWindowController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
+    private ChoiceBox<?> countryChoiceBox;
+
+    @FXML
+    private ComboBox<?> cityComboBox;
+
+    @FXML
+    private TextArea weatherTextArea;
+
+    @FXML
+    void showButton() {
 
     }
 }
