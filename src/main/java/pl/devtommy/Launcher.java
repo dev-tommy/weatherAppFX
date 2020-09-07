027c381028e4f05f5c694c7e1cbd824fe47194d0
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-09-07 18:23:49 +0200

    Add style to MainWindow

--- src/main/java/pl/devtommy/Launcher.java
+++ src/main/java/pl/devtommy/Launcher.java
@@ -15,6 +15,7 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("MainWindow"));
+        scene.getStylesheets().add(loadStyle("style"));
         stage.setScene(scene);
         stage.setTitle("Prognoza pogody");
         stage.setResizable(false);
@@ -24,6 +25,10 @@ public class Launcher extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+
+    private static String loadStyle(String style) throws IOException {
+        return String.valueOf(Launcher.class.getResource(style + ".css"));
     }
 
     public static void main(String[] args) {
