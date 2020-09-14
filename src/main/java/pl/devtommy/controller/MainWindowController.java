be07aa574d88f83b980c1379b850ec1afa52552b
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-09-14 12:20:48 +0200

    Add icons zoom in/out

--- src/main/java/pl/devtommy/controller/MainWindowController.java
+++ src/main/java/pl/devtommy/controller/MainWindowController.java
@@ -1,26 +1,149 @@
 package pl.devtommy.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class MainWindowController {
 
     @FXML
+    private ImageView refreshLeftImageView;
+
+    @FXML
+    private ImageView closeAppImageView;
+
+    @FXML
+    private ImageView refreshRightImageView;
+
+    @FXML
+    private ImageView leftCityImageView;
+
+    @FXML
+    private ImageView leftCountryImageView;
+
+    @FXML
+    private ImageView rightCityImageView;
+
+    @FXML
+    private ImageView rightCountryImageView;
+
+    @FXML
+    void closeAppOnMouseEntered(MouseEvent event) {
+        zoomIn(closeAppImageView, 10);
+    }
+
+    @FXML
+    void closeAppOnMouseExited(MouseEvent event) {
+        zoomOut(closeAppImageView, 10);
+    }
+
+    @FXML
     void closeApp(MouseEvent event) {
-        System.out.println("Mouse Exited");
         System.exit(0);
     }
 
     @FXML
-    private ComboBox<?> cityComboBox;
+    void refreshLeftOnMouseEntered(MouseEvent event) {
+        zoomIn(refreshLeftImageView, 10);
+    }
 
     @FXML
-    private TextArea weatherTextArea;
+    void refreshLeftOnMouseExited(MouseEvent event) {
+        zoomOut(refreshLeftImageView, 10);
+    }
 
     @FXML
-    void showButton() {
+    void refreshLeftWeather(MouseEvent event) {
+    }
+
+    @FXML
+    void refreshRightOnMouseEntered(MouseEvent event) {
+        zoomIn(refreshRightImageView, 10);
+    }
+
+    @FXML
+    void refreshRightOnMouseExited(MouseEvent event) {
+        zoomOut(refreshRightImageView, 10);
+    }
+
+    @FXML
+    void refreshRightWeather(MouseEvent event) {
+    }
+
+    @FXML
+    void changeLeftCity(MouseEvent event) {
 
     }
+
+    @FXML
+    void changeLeftCountry(MouseEvent event) {
+
+    }
+
+    @FXML
+    void changeRightCity(MouseEvent event) {
+
+    }
+
+    @FXML
+    void changeRightCountry(MouseEvent event) {
+
+    }
+
+    @FXML
+    void leftCityOnMouseEntered(MouseEvent event) {
+        zoomIn(leftCityImageView, 10);
+    }
+
+    @FXML
+    void leftCityOnMouseExited(MouseEvent event) {
+        zoomOut(leftCityImageView, 10);
+    }
+
+    @FXML
+    void leftCountryOnMouseEntered(MouseEvent event) {
+        zoomIn(leftCountryImageView, 10);
+    }
+
+    @FXML
+    void leftCountryOnMouseExited(MouseEvent event) {
+        zoomOut(leftCountryImageView, 10);
+    }
+
+    @FXML
+    void rightCityOnMouseEntered(MouseEvent event) {
+        zoomIn(rightCityImageView, 10);
+    }
+
+    @FXML
+    void rightCityOnMouseExited(MouseEvent event) {
+        zoomOut(rightCityImageView, 10);
+    }
+
+    @FXML
+    void rightCountryOnMouseEntered(MouseEvent event) {
+        zoomIn(rightCountryImageView, 10);
+    }
+
+    @FXML
+    void rightCountryOnMouseExited(MouseEvent event) {
+        zoomOut(rightCountryImageView, 10);
+    }
+
+
+    private void zoomIn(ImageView imageView, int size){
+        imageView.setFitWidth(imageView.getFitWidth()+2*size);
+        imageView.setFitHeight(imageView.getFitHeight()+2*size);
+        imageView.setLayoutX(imageView.getLayoutX()-size);
+        imageView.setLayoutY(imageView.getLayoutY()-size);
+    }
+
+    private void zoomOut(ImageView imageView, int size){
+        imageView.setFitWidth(imageView.getFitWidth()-2*size);
+        imageView.setFitHeight(imageView.getFitHeight()-2*size);
+        imageView.setLayoutX(imageView.getLayoutX()+size);
+        imageView.setLayoutY(imageView.getLayoutY()+size);
+    }
 }
+
+
