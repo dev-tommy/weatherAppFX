0c0bf88cfdf33f7b87ea7eb3e92055a4597d63bb
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-09-16 13:07:00 +0200

    Add owm.japis and java.sql requires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module pl.devtommy {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires owm.japis;
+    requires java.sql;
 
     opens pl.devtommy;
     opens pl.devtommy.controller;
