9c1f1d018123077ea20cf8d95ef2fb55a2b67442
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-09-24 19:59:00 +0200

    Change methods to one with City argument

--- src/main/java/pl/devtommy/model/WeatherProvider.java
+++ src/main/java/pl/devtommy/model/WeatherProvider.java
@@ -1,6 +1,5 @@
 package pl.devtommy.model;
 
 public interface WeatherProvider {
-    public OneDayWeather getCurrentWeatherByCityName(String cityName);
-    public OneDayWeather getCurrentWeatherByCityId(int cityId);
+    public OneDayWeather getCurrentWeatherByCity(City city);
 }
