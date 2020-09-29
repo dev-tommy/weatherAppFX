b8a250b3609bc8b6f6247357df88c9f3bdedc169
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-09-29 17:27:49 +0200

    Add constructor with WeatherProviderManager

--- src/main/java/pl/devtommy/controller/SelectCityLocationController.java
+++ src/main/java/pl/devtommy/controller/SelectCityLocationController.java
@@ -1,11 +1,22 @@
 package pl.devtommy.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import pl.devtommy.WeatherProviderManager;
 import pl.devtommy.model.City;
 
 public class SelectCityLocationController {
+
+    WeatherProviderManager weatherProviderManager;
+    City selectedCity;
+
+    public SelectCityLocationController(WeatherProviderManager weatherProviderManager) {
+        this.weatherProviderManager = weatherProviderManager;
+    }
 
     @FXML
     private TextField idLabel;
