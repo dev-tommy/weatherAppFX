38b4c866703e9933b274dfead5f0599e9639f8a4
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-04 14:17:29 +0200

    Add mainStageInit() with MainWindow setup

--- src/main/java/pl/devtommy/view/ViewFactory.java
+++ src/main/java/pl/devtommy/view/ViewFactory.java
@@ -1,11 +1,43 @@
 package pl.devtommy.view;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import pl.devtommy.Launcher;
 import pl.devtommy.WeatherManager;
+import pl.devtommy.controller.BaseController;
+
+import java.io.IOException;
 
 public class ViewFactory {
     private WeatherManager weatherManager;
+    private Stage ownerStage;
 
-    public ViewFactory(WeatherManager weatherManager) {
+    public ViewFactory(Stage stage, WeatherManager weatherManager) {
         this.weatherManager = weatherManager;
+        this.ownerStage = stage;
+    }
+
+    private void mainStageInit(BaseController baseController) {
+        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("view/fxml/" + baseController.getFxmlName()));
+        fxmlLoader.setController(baseController);
+
+        Parent parent;
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        scene.setFill(Color.TRANSPARENT);
+        ownerStage.setScene(scene);
+        ownerStage.initStyle(StageStyle.TRANSPARENT);
+        ownerStage.setResizable(false);
+        ownerStage.show();
     }
 }
