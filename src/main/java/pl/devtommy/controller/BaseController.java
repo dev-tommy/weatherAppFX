0dfd5d042340e320b86cfca74a18d4e630f39113
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-08 12:02:08 +0200

    Add second constructor BaseController(WeatherManager)

--- src/main/java/pl/devtommy/controller/BaseController.java
+++ src/main/java/pl/devtommy/controller/BaseController.java
@@ -1,17 +1,23 @@
 package pl.devtommy.controller;
 
 import pl.devtommy.WeatherManager;
+import pl.devtommy.model.WeatherProvider;
 import pl.devtommy.view.ViewFactory;
 
 public abstract class BaseController {
+    protected WeatherProvider[] weathers;
     protected WeatherManager weatherManager;
     protected ViewFactory viewFactory;
     private String fxmlName;
 
-    public BaseController(WeatherManager weatherManager, ViewFactory viewFactory, String fxmlName) {
-        this.weatherManager = weatherManager;
+    public BaseController(WeatherProvider[] weathers, ViewFactory viewFactory, String fxmlName) {
+        this.weathers = weathers;
         this.viewFactory = viewFactory;
         this.fxmlName = fxmlName;
+    }
+
+    public BaseController(WeatherManager weatherManager) {
+        this.weatherManager = weatherManager;
     }
 
     public String getFxmlName() {
