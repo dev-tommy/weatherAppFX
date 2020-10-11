57c820c0fcce6ca8c613345c7d94a3c97a9fb8ae
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-11 18:40:07 +0200

    Add show date, main weather condition and temperature

--- src/main/java/pl/devtommy/controller/ForecastDayWindowController.java
+++ src/main/java/pl/devtommy/controller/ForecastDayWindowController.java
@@ -7,8 +7,9 @@ import javafx.scene.image.ImageView;
 import pl.devtommy.model.CityWeather;
 import pl.devtommy.model.DayWeather;
 
-public class ForecastDayWindowController {
-    private  OneDayWeather forecastDay;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.ResourceBundle;
 
 public class ForecastDayWindowController implements Initializable {
     private DayWeather forecastDay;
@@ -31,4 +32,30 @@ public class ForecastDayWindowController implements Initializable {
 
     @FXML
     private Label forecastTempLabel;
+
+    private void updateDate() {
+        LocalDateTime forecastDate = forecastDay.getDate();
+        String day = String.valueOf(forecastDate.getDayOfMonth());
+        String dayOfWeek = forecastDate.getDayOfWeek().name().substring(0,3);
+
+        String date = dayOfWeek + " " + day;
+        forecastDayOfMonthLabel.setText(date);
+    }
+
+    private void refreshWeather() {
+        updateWeatherView();
+        updateWeatherImages();
+    }
+
+    private void updateWeatherImages() {
+        String forecastWeatherMainCondition = forecastDay.getMainCondition();
+        forecastWeatherImageView.setImage(CityWeather.getWeatherImage(forecastWeatherMainCondition));
+    }
+
+    private void updateWeatherView() {
+        forecastTempLabel.setText(forecastDay.getTemp());
+    }
+
+
+
 }
