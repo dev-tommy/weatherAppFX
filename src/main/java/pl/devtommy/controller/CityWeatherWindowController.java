c0314d7cfcc3e659b4e54ea644f9980822510a26
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-11 18:41:54 +0200

    Add show weather data

--- src/main/java/pl/devtommy/controller/CityWeatherWindowController.java
+++ src/main/java/pl/devtommy/controller/CityWeatherWindowController.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import pl.devtommy.model.CityWeather;
 import pl.devtommy.model.City;
@@ -12,23 +11,18 @@ import pl.devtommy.model.DayWeather;
 import pl.devtommy.view.ViewFactory;
 
 import java.net.URL;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ResourceBundle;
 
 public class CityWeatherWindowController implements Initializable {
 
-    private int FORECAST_DAYS_AMOUNT = 4;
-    private WeatherManager cityWeather;
-
-    public CityWeatherWindowController(WeatherManager cityWeather, int maxForecastDays) {
-        this.cityWeather = cityWeather;
-        this.FORECAST_DAYS_AMOUNT = maxForecastDays;
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        addForecastDays();
-    }
-
+    private int FORECAST_DAYS_AMOUNT;
+    private int zoomIconSize = 2;
+    private CityWeather cityWeather;
+    private City city;
+    private DayWeather dayWeather;
 
     @FXML
     private HBox forecastHBox;
@@ -69,61 +63,128 @@ public class CityWeatherWindowController implements Initializable {
     @FXML
     private Label tempMinMaxLabel;
 
-    @FXML
-    private ImageView closeAppImageView;
+    public CityWeatherWindowController(CityWeather cityWeather, int maxForecastDays) {
+        this.cityWeather = cityWeather;
+        this.FORECAST_DAYS_AMOUNT = maxForecastDays;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        getCityLocation();
+        refreshWeather();
+    }
 
 
 
     @FXML
-    void changeCity(MouseEvent event) {
-
+    void changeCity() {
+        getCity();
     }
 
     @FXML
-    void changeCityOnMouseEntered(MouseEvent event) {
-
+    void refreshWeather() {
+        setCityLocation();
+        updateDates();
+        updateWeatherView();
+        updateWeatherImages();
+        updateForecastWeather();
     }
 
     @FXML
-    void changeCityOnMouseExited(MouseEvent event) {
-
+    void changeCityOnMouseEntered() {
+        zoomIn(cityImageView, zoomIconSize);
     }
 
     @FXML
-    void closeApp(MouseEvent event) {
-
+    void changeCityOnMouseExited() {
+        zoomOut(cityImageView, zoomIconSize);
     }
 
     @FXML
-    void closeAppOnMouseEntered(MouseEvent event) {
-
+    void refreshOnMouseEntered() {
+        zoomIn(refreshImageView, zoomIconSize);
     }
 
     @FXML
-    void closeAppOnMouseExited(MouseEvent event) {
+    void refreshOnMouseExited() {
+        zoomOut(refreshImageView, zoomIconSize);
+    }
+
+    private void updateDates() {
+        LocalDateTime today = LocalDateTime.now();
+        int day = today.getDayOfMonth();
+        Month month = today.getMonth();
+        DayOfWeek dayOfWeek = today.getDayOfWeek();
+
+        monthLabel.setText(month.name().substring(0,1).toUpperCase() + month.name().substring(1,3).toLowerCase());
+        dayLabel.setText(dayOfWeek.name().substring(0,1).toUpperCase() + dayOfWeek.name().substring(1,3).toLowerCase() +
+                " " + day);
+    }
+
+    private void setCityLocation() {
+        cityWeather.setCity(city);
+    }
+
+    private void getCityLocation() {
+        city = cityWeather.getCityLocation();
+    }
+
+    private void updateForecastWeather() {
+        forecastHBox.getChildren().clear();
+        addForecastDays();
 
     }
 
-    @FXML
-    void refreshOnMouseEntered(MouseEvent event) {
-
+    private void updateWeatherImages() {
+        String currentWeatherMainCondition = dayWeather.getMainCondition();
+        currentImageView.setImage(CityWeather.getWeatherImage(currentWeatherMainCondition));
     }
 
-    @FXML
-    void refreshOnMouseExited(MouseEvent event) {
+    private void updateWeatherView() {
+        dayWeather = cityWeather.getCurrentLeftCityWeather();
 
+        System.out.println(dayWeather);
+
+        cityLabel.setText(dayWeather.getName());
+        countryLabel.setText(dayWeather.getCountry());
+        currentTempLabel.setText( dayWeather.getTemp());
+        humidityLabel.setText(dayWeather.getHumidity());
+        pressureLabel.setText(dayWeather.getPressure());
+        tempMinMaxLabel.setText(
+                dayWeather.getTempMin() +
+                        " / " +
+                        dayWeather.getTempMax()
+        );
+
+        describeLabel.setText(dayWeather.getDescription());
     }
-
-    @FXML
-    void refreshWeather(MouseEvent event) {
-
-    }
-
 
     private void addForecastDays() {
-        for (int i=0; i< FORECAST_DAYS_AMOUNT; i++){
-            OneDayWeather forecastDay = cityWeather.getForecastLeftCityWeather()[i];
+        for (int i=0; i < FORECAST_DAYS_AMOUNT; i++){
+            DayWeather forecastDay = cityWeather.getForecastWeather()[i];
             forecastHBox.getChildren().add(ViewFactory.addForecastDay(forecastDay));
         }
     }
+
+    private void getCity() {
+        cityWeather.setSelectedCity(null);
+        ViewFactory.showSelectCityLocationWindow(cityWeather);
+        city = cityWeather.getSelectedCity();
+        refreshWeather();
+    }
+
+    private void zoomIn(ImageView imageView, int size){
+        imageView.setFitWidth(imageView.getFitWidth()+2*size);
+        imageView.setFitHeight(imageView.getFitHeight()+2*size);
+        imageView.setLayoutX(imageView.getLayoutX()-size);
+        imageView.setLayoutY(imageView.getLayoutY()-size);
+    }
+
+    private void zoomOut(ImageView imageView, int size){
+        imageView.setFitWidth(imageView.getFitWidth()-2*size);
+        imageView.setFitHeight(imageView.getFitHeight()-2*size);
+        imageView.setLayoutX(imageView.getLayoutX()+size);
+        imageView.setLayoutY(imageView.getLayoutY()+size);
+    }
+
 }
