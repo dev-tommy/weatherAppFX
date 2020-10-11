b8138e1245ec7ec1509c41023909497616f6c9dd
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-11 18:39:06 +0200

    Implements Initializable interface

--- src/main/java/pl/devtommy/controller/ForecastDayWindowController.java
+++ src/main/java/pl/devtommy/controller/ForecastDayWindowController.java
@@ -1,6 +1,7 @@
 package pl.devtommy.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import pl.devtommy.model.CityWeather;
