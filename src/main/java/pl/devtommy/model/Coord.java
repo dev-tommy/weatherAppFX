b922cbe7e82c912874eac85c985111174cf9ba2d
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-17 12:10:29 +0200

    Add Override equals(), hashCode() and toString()

--- src/main/java/pl/devtommy/model/Coord.java
+++ src/main/java/pl/devtommy/model/Coord.java
@@ -1,10 +1,13 @@
 package pl.devtommy.model;
 
+import java.util.Objects;
+
 public class Coord {
     private double lon;
     private double lat;
 
     public Coord() {
+        //for Gson
     }
 
     public Coord(double lat, double lon) {
@@ -18,5 +21,27 @@ public class Coord {
 
     public double getLat() {
         return lat;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coord coord = (Coord) o;
+        return Double.compare(coord.lon, lon) == 0 &&
+                Double.compare(coord.lat, lat) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lon, lat);
+    }
+
+    @Override
+    public String toString() {
+        return "Coord{" +
+                "lon=" + lon +
+                ", lat=" + lat +
+                '}';
     }
 }
