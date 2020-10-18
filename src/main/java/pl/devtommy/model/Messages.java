6cdd988b765a72b15cba35af3e9aee46ba11c0c4
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-18 12:22:13 +0200

    Fix location word

--- src/main/java/pl/devtommy/model/Messages.java
+++ src/main/java/pl/devtommy/model/Messages.java
@@ -1,8 +1,7 @@
 package pl.devtommy.model;
 
 public class Messages {
-
     public static final String WRONG_CITY_DATA_MESSAGE = "Wrong city data!";
-    public static final String FOUND_LOCATIONS_MESSAGE = "Found locactions: ";
+    public static final String FOUND_LOCATIONS_MESSAGE = "Found locations: ";
     public static final String WRONG_API_KEY_MESSAGE = "Wrong API key! API call gave error: 401 - Unauthorized";
 }
