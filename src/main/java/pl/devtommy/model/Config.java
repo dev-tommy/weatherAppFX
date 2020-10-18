d34844a1eedbc9494f5941b8d65086c161b87193
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-18 19:39:25 +0200

    Add createDefaultProperties()

--- src/main/java/pl/devtommy/model/Config.java
+++ src/main/java/pl/devtommy/model/Config.java
@@ -6,7 +6,11 @@ import java.io.*;
 import java.util.Properties;
 
 public class Config {
+    private static int citiesNumber = 2;
+    private static String defaultCityId = "251280";
     private static String configPath;
+    private static String apiKey;
+    private static City[] cities;
 
     public Config(String configPath) {
         this.configPath = configPath;
@@ -42,5 +46,15 @@ public class Config {
         }
 
         return apiKey;
+    }
+
+    private static Properties createDefaultProperties() {
+        apiKey = ViewFactory.getApiDialog();
+        Properties prop = new Properties();
+        prop.setProperty("owm.api.key", apiKey);
+        prop.setProperty("cities.number", String.valueOf(citiesNumber));
+        prop.setProperty("city.0.id", "7533329");
+        prop.setProperty("city.1.id", "7533329");
+        return prop;
     }
 }
