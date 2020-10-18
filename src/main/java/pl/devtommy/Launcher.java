c12e9679856436cec6836811114bc66221469e92
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-18 13:12:43 +0200

    Add config.setPath()

--- src/main/java/pl/devtommy/Launcher.java
+++ src/main/java/pl/devtommy/Launcher.java
@@ -3,6 +3,7 @@ package pl.devtommy;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import pl.devtommy.model.Config;
+import pl.devtommy.model.Paths;
 import pl.devtommy.model.WeatherProvider;
 import pl.devtommy.model.weatherproviders.OWMProvider;
 import pl.devtommy.view.ViewFactory;
@@ -10,6 +11,7 @@ import pl.devtommy.view.ViewFactory;
 public class Launcher extends Application {
     public static Config config = new Config();
     public static void main(String[] args) {
+        config.setPath(Paths.CONFIG_PROPERTIES_PATH);
         launch();
     }
 
