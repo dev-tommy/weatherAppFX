1f8764868262aad5d5829644308dd99d0ca16cfa
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-18 20:51:09 +0200

    Add '/' in IMAGE_WEATHER_PARENT_PATH

--- src/main/java/pl/devtommy/model/Paths.java
+++ src/main/java/pl/devtommy/model/Paths.java
@@ -3,5 +3,5 @@ package pl.devtommy.model;
 public class Paths {
     public static final String CONFIG_PROPERTIES_PATH = "config.properties";
     public static final String CITY_LIST_JSON_PATH = "/pl/devtommy/json/city.list.json";
-    public static final String IMAGE_WEATHER_PARENT_PATH = "/pl/devtommy/icon/weather";
+    public static final String IMAGE_WEATHER_PARENT_PATH = "/pl/devtommy/icon/weather/";
 }
