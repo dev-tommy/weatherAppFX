97790594d503ed1f6a324e55f4d06adda66f87ed
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-18 19:27:47 +0200

    Add new messages

--- src/main/java/pl/devtommy/model/Messages.java
+++ src/main/java/pl/devtommy/model/Messages.java
@@ -5,4 +5,7 @@ public class Messages {
     public static final String FOUND_LOCATIONS_MESSAGE = "Found locations: ";
     public static final String WRONG_API_KEY_MESSAGE = "Wrong API key! API call gave error: 401 - Unauthorized";
     public static final String FILE_ERROR_MESSAGE = "File error";
+    public static final String CREATE_PROPERTIES_FILE_ERROR_MESSAGE = "Create properties file error";
+    public static final String STORE_PROPERTIES_ERROR_MESSAGE = "Store properties error";
+    public static final String DEFAULT_SETTINGS_MESSAGE = "default settings";
 }
