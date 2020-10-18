4fddff48687370661646d50765cc9e15bc0737d2
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-18 20:13:13 +0200

    Add NO_API_KEY_MESSAGE

--- src/main/java/pl/devtommy/model/Messages.java
+++ src/main/java/pl/devtommy/model/Messages.java
@@ -4,6 +4,7 @@ public class Messages {
     public static final String WRONG_CITY_DATA_MESSAGE = "Wrong city data!";
     public static final String FOUND_LOCATIONS_MESSAGE = "Found locations: ";
     public static final String WRONG_API_KEY_MESSAGE = "Wrong API key! API call gave error: 401 - Unauthorized";
+    public static final String NO_API_KEY_MESSAGE = "No API key! The application has been closed";
     public static final String FILE_ERROR_MESSAGE = "File error";
     public static final String CREATE_PROPERTIES_FILE_ERROR_MESSAGE = "Create properties file error";
     public static final String STORE_PROPERTIES_ERROR_MESSAGE = "Store properties error";
